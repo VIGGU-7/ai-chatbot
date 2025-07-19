451b5ae9dfcff3d03de541607b6021868b495460
VIGGU <vigneshpatlavath@gmail.com>
2025-07-19 17:09:18 +0530

    add user middleware

---
+++ backend/src/middleware/user.middleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+import { User } from '../models/user.model.js'
+import asyncHandler from '../utils/asyncHandler.js'
+export const userMiddleware=asyncHandler(async(req,res,next)=>{
+const {token}=req.cookies
+if(!token){
+    return res.status(401).json({
+        success:false,
+        message:"No auth token has been provided"
+    })
+}
+const decoded=jwt.verify(token,process.env.JWT_SECRET)
+const user=await User.findById(decoded._id).select("-password")
+if(!user){
+    return res.status(401).json({
+        success:false,
+        message:"No user found authenticated"
+    })
+}
+req.user=user;
+next();
+})
