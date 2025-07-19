738106ddbfa464941d3a8f5c4dc5f1f2b3c29567
VIGGU <vigneshpatlavath@gmail.com>
2025-07-19 17:11:34 +0530

    added logout route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { login,signup } from "../controllers/user.controller.js";
+import { login,logout,signup } from "../controllers/user.controller.js";
+import { userMiddleware } from "../middleware/user.middleware.js";
 const router=Router()
 router.post("/login",login)
 router.post("/signup",signup)
+router.get("/logout",userMiddleware,logout)
 export default router;
