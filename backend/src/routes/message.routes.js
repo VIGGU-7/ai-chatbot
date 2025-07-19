34329294021fd0156e5a12a07564308da7e95b84
VIGGU <vigneshpatlavath@gmail.com>
2025-07-19 21:53:04 +0530

    added routes

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { getMessagesBysessionId, sendMessage } from "../controllers/message.controller.js";
 import { userMiddleware } from "../middleware/user.middleware.js";
 const router=Router()
 router.post("/send",userMiddleware,sendMessage)
-
+router.post("/send/:sessionId",userMiddleware,sendMessage)
+router.get("/chat/:id",userMiddleware,getMessagesBysessionId)
 export default router;
