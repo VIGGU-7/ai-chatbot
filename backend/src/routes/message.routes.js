5e9743a8142cfc1a80b772cded8d905f28660815
VIGGU <vigneshpatlavath@gmail.com>
2025-07-21 13:14:25 +0530

    added search route

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getMessagesBysessionId, getsessionhistory, sendMessage } from "../controllers/message.controller.js";
+import { getMessagesBysessionId, getsessionhistory, searchSession, sendMessage } from "../controllers/message.controller.js";
 import { userMiddleware } from "../middleware/user.middleware.js";
 const router=Router()
 router.post("/send",userMiddleware,sendMessage)
 router.post("/send/:sessionId",userMiddleware,sendMessage)
 router.get("/chat/:id",userMiddleware,getMessagesBysessionId)
 router.get("/history",userMiddleware,getsessionhistory)
+router.get("/search",userMiddleware,searchSession)
 export default router;
